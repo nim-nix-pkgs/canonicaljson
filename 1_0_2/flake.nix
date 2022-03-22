508ee257e18cd11c76b3c0ce37c99f5182529dc6
hugosenari <hugosenari@gmail.com>
2022-03-22 00:25:29 -0300

    chore: re-index ../nimpkgs/c/canonicaljson/1_0_2

--- 1_0_2/flake.nix
+++ 1_0_2/flake.nix
@@ -1,5 +1,5 @@
 {
-  description = ''Canonical JSON according to RFC8785'';
+  description = ''canonicaljson'';
 
   inputs.flakeNimbleLib.owner = "riinr";
   inputs.flakeNimbleLib.ref   = "master";
@@ -9,7 +9,7 @@
   
   inputs.src-canonicaljson-1_0_2.flake = false;
   inputs.src-canonicaljson-1_0_2.owner = "jackhftang";
-  inputs.src-canonicaljson-1_0_2.ref   = "refs/tags/1.0.2";
+  inputs.src-canonicaljson-1_0_2.ref   = "1_0_2";
   inputs.src-canonicaljson-1_0_2.repo  = "canonicaljson.nim";
   inputs.src-canonicaljson-1_0_2.type  = "github";
   
